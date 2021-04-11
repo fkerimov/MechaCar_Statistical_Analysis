2620e08c3e0e4ddfbb40553408c41be3a95059b3
Farhad Kerimov <farhad.kerimov@gmail.com>
2021-04-11 01:08:55 -0400

    Adding R Script for coil PSI stats

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,8 @@
-# Load dplyr package
+# Load package
 library(dplyr)
 
 # Import and read the csv
-MechaCar_mpg_table <- read.csv("resources/MechaCar_mpg.csv")
+MechaCar_mpg_table <- read.csv("resources/MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 
 # Perform multiple linear regression 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg_table)
@@ -10,3 +10,23 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Get the p-value and r-squared
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD, data = MechaCar_mpg_table))
+
+# Import and read the suspension_coil csv
+suspension_coil_table <- read.csv("resources/Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+
+# Get mean, median, variance, and SD of the PSI column
+total_summary <- suspension_coil_table %>% 
+  summarize(Mean = mean(PSI), 
+            Median = median(PSI), 
+            Variance = var(PSI),
+            SD = sd(PSI),
+            .groups = "keep")
+
+# Group manufacturing lots by center and spread of of the PSI column
+lot_summary <- suspension_coil_table %>% 
+  group_by(Manufacturing_Lot) %>% 
+  summarize(Mean = mean(PSI), 
+            Median = median(PSI), 
+            Variance = var(PSI),
+            SD = sd(PSI),
+            .groups = "keep")
